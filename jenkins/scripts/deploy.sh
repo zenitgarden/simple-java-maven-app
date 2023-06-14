4e7993af102073bac9935287373ecd438541952f
zenit <zenitharryan@gmail.com>
2023-06-14 08:31:45 +0700

    add deploy.sh

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,7 @@
+echo 'Deploying to the EC2 instance'
+echo 'The following Maven command builds the project and packages the resulting JAR file into the target directory.'
+mvn package
+
+echo 'Run the project'
+echo 'The following Maven command run the jar file in target/my-app-1.0-SNAPSHOT.jar'
+java -jar target/my-app-1.0-SNAPSHOT.jar
